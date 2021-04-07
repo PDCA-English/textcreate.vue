3423778775f25f6b67ec66a009de6e0341edf046
Takuya <r28.6sa@gmail.com>
2021-04-08 08:13:34 +0900

    v-for is a problem

--- src/views/Text.vue
+++ src/views/Text.vue
@@ -1,114 +1,26 @@
 <template>
   <div id="app">
-    <div class="frontHeader" :style="[bgColor, titleColor]">
-      <div class="frontHeaderLeft">
-        <p>{{chapterContents[0][0]}}</p>
-        <p :style="titleColor">{{chapterContents[0][1]}}</p>
-      </div>
-      <div class="frontHeaderRight" :style="titleColor">
-        <p>{{chapterContents[0][2]}}</p>
-        <p>{{chapterContents[0][3]}}</p>
-      </div>
-    </div>
-    <div class="frontBody">
-      <div class="eachSentence" v-for="eachContents in chapterContents" v-bind:key="eachContents.id">
-        <div class="eachHead" v-for="eachSentence in eachContents" v-bind:key="eachSentence.id">
-          <p>□</p>
-          <p>{{eachSentence[0]}}</p>
+    <div id="content" v-for="chapterContent in chapterContents" v-bind:key="chapterContent.id">
+      <div class="frontHeader" :style="[bgColor, titleColor]">
+        <div class="frontHeaderLeft">
+          <p>{{chapterContent[0]}}</p>
+          <p :style="titleColor">{{chapterContent[1]}}</p>
         </div>
-        <div class="eachContent">
-          <p v-html="chapterContents[0][5][1]"></p>
-          <p>{{chapterContents[0][5][2]}}</p>
+        <div class="frontHeaderRight" :style="titleColor">
+          <p>{{chapterContent[2]}}</p>
+          <p>{{chapterContent[3]}}</p>
         </div>
       </div>
-      <div class="eachSentence">
-        <div class="eachHead">
-          <p>□</p>
-          <p>02</p>
-        </div>
-        <div class="eachContent">
-          <p>{{chapterContents[0][6][1]}}</p>
-          <p>{{chapterContents[0][6][2]}}</p>
-        </div>
-      </div>
-      <div class="eachSentence">
-        <div class="eachHead">
-          <p>□</p>
-          <p>03</p>
-        </div>
-        <div class="eachContent">
-          <p>{{chapterContents[0][7][1]}}</p>
-          <p>{{chapterContents[0][7][2]}}</p>
-        </div>
-      </div>
-      <div class="eachSentence">
-        <div class="eachHead">
-          <p>□</p>
-          <p>04</p>
-        </div>
-        <div class="eachContent">
-          <p>{{chapterContents[0][8][1]}}</p>
-          <p>{{chapterContents[0][8][2]}}</p>
-        </div>
-      </div>
-      <div class="eachSentence">
-        <div class="eachHead">
-          <p>□</p>
-          <p>05</p>
-        </div>
-        <div class="eachContent">
-          <p>{{chapterContents[0][9][1]}}</p>
-          <p>{{chapterContents[0][9][2]}}</p>
-        </div>
-      </div>
-      <div class="eachSentence">
-        <div class="eachHead">
-          <p>□</p>
-          <p>06</p>
-        </div>
-        <div class="eachContent">
-          <p>{{chapterContents[0][10][1]}}</p>
-          <p>{{chapterContents[0][10][2]}}</p>
-        </div>
-      </div>
-      <div class="eachSentence">
-        <div class="eachHead">
-          <p>□</p>
-          <p>07</p>
-        </div>
-        <div class="eachContent">
-          <p>{{chapterContents[0][11][1]}}</p>
-          <p>{{chapterContents[0][11][2]}}</p>
-        </div>
-      </div>
-      <div class="eachSentence">
-        <div class="eachHead">
-          <p>□</p>
-          <p>08</p>
-        </div>
-        <div class="eachContent">
-          <p>{{chapterContents[0][12][1]}}</p>
-          <p>{{chapterContents[0][12][2]}}</p>
-        </div>
-      </div>
-      <div class="eachSentence">
-        <div class="eachHead">
-          <p>□</p>
-          <p>09</p>
-        </div>
-        <div class="eachContent">
-          <p>{{chapterContents[0][13][1]}}</p>
-          <p>{{chapterContents[0][13][2]}}</p>
-        </div>
-      </div>
-      <div class="eachSentence">
-        <div class="eachHead">
-          <p>□</p>
-          <p>10</p>
-        </div>
-        <div class="eachContent">
-          <p>{{chapterContents[0][14][1]}}</p>
-          <p>{{chapterContents[0][14][2]}}</p>
+      <div class="frontBody">
+        <div class="eachSentence" v-for="firstPageContent in firstPageContents" v-bind:key="firstPageContent.id">
+          <div class="eachHead">
+            <p>□</p>
+            <p>{{firstPageContent[0]}}</p>
+          </div>
+          <div class="eachContent">
+            <p v-html="firstPageContent[1]"></p>
+            <p>{{firstPageContent[2]}}</p>
+          </div>
         </div>
       </div>
     </div>
