5b32e756789e9256b25732d8890a7974eae77d17
Takuya <r28.6sa@gmail.com>
2021-04-11 17:04:53 +0900

    trying to use PDF.js but dont know how

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <template>
   <div id="app">
     <router-view/>
+    <PDFJSViewer :path="`${path}`" :fileName="`${name}`"/>
   </div>
 </template>
 
 
+<script>
+import PDFJSViewer from './components/PDFJSViewer'
 
+export default {
+  name: 'app',
+  components: {
+    PDFJSViewer
+  },
+  data () {
+    return {
+      name: 'demo.pdf', //change which pdf file loads
+      path: 'lib/pdfjs-2.3.200-dist/web/viewer.html' //path of the PDF.js viewer.html
+    }
+  }
+}
+</script>
 
 <style>
 #app {
--- src/views/Text.vue
+++ src/views/Text.vue
@@ -1,69 +1,79 @@
 <template>
   <div id="app">
     <button class="btn" @click="downloadPDF">Download PDF</button>
-    <div id="content" ref="pdf" v-for="chapterContent in chapterContents" v-bind:key="chapterContent.id">
-      <div class="frontHeader" :style="[bgColor, titleColor]">
-        <div class="frontHeaderLeft">
-          <p>{{chapterContent[0]}}</p>
-          <p :style="titleColor">{{chapterContent[1]}}</p>
+    <div id="content" ref="pdf">
+      <div
+        v-for="chapterContent in chapterContents"
+        v-bind:key="chapterContent.id"
+      >
+        <div class="frontHeader" :style="[bgColor, titleColor]">
+          <div class="frontHeaderLeft">
+            <p>{{ chapterContent[0] }}</p>
+            <p :style="titleColor">{{ chapterContent[1] }}</p>
+          </div>
+          <div class="frontHeaderRight" :style="titleColor">
+            <p>{{ chapterContent[2] }}</p>
+            <p>{{ chapterContent[3] }}</p>
+          </div>
         </div>
-        <div class="frontHeaderRight" :style="titleColor">
-          <p>{{chapterContent[2]}}</p>
-          <p>{{chapterContent[3]}}</p>
+        <div class="frontBody">
+          <div
+            class="eachSentence"
+            v-for="firstPageContent in chapterContent.slice(5, 15)"
+            v-bind:key="firstPageContent.id"
+          >
+            <div class="eachHead">
+              <p>□</p>
+              <p>{{ firstPageContent[0] }}</p>
+            </div>
+            <div class="eachContent">
+              <p class="firstEsentence" v-html="firstPageContent[1]"></p>
+              <p>{{ firstPageContent[2] }}</p>
+            </div>
+          </div>
         </div>
+        <footer>24/7English</footer>
+        <div class="pagebreak"></div>
+        <div class="backHeader" :style="[bgColor, titleColor]">
+          <div class="backTitle">
+            {{ chapterContent[2] }}
+          </div>
+          <div class="backNum">
+            {{ chapterContent[0] }}
+          </div>
+        </div>
+        <div class="backBody">
+          <div
+            class="eachSentence"
+            v-for="secondPageContent in chapterContent.slice(5, 15)"
+            v-bind:key="secondPageContent.id"
+          >
+            <div class="eachHead">
+              <p>□</p>
+              <p>{{ secondPageContent[0] }}</p>
+            </div>
+            <div class="eachContent">
+              <p>{{ secondPageContent[2] }}</p>
+            </div>
+            <div class="eachHint">
+              <p>{{ secondPageContent[3] }}</p>
+              <p>{{ secondPageContent[4] }}</p>
+            </div>
+          </div>
+          <div class="topic">
+            {{ chapterContent[4] }}
+          </div>
+        </div>
+        <footer>24/7English</footer>
+        <div class="pagebreak"></div>
       </div>
-      <div class="frontBody">
-        <div class="eachSentence" v-for="firstPageContent in chapterContent.slice(5, 15)" v-bind:key="firstPageContent.id">
-          <div class="eachHead">
-            <p>□</p>
-            <p>{{firstPageContent[0]}}</p>
-          </div>
-          <div class="eachContent">
-            <p class="firstEsentence" v-html="firstPageContent[1]"></p>
-            <p>{{firstPageContent[2]}}</p>
-          </div>
-        </div>
-      </div>
-      <footer>24/7English</footer>
-      <div class="pagebreak"></div>
-      <div class="backHeader" :style="[bgColor, titleColor]">
-        <div class="backTitle">
-          {{chapterContent[2]}}
-        </div>
-        <div class="backNum">
-          {{chapterContent[0]}}
-        </div>
-      </div>
-      <div class="backBody">
-        <div class="eachSentence" v-for="secondPageContent in chapterContent.slice(5, 15)" v-bind:key="secondPageContent.id">
-          <div class="eachHead">
-            <p>□</p>
-            <p>{{secondPageContent[0]}}</p>
-          </div>
-          <div class="eachContent">
-            <p>{{secondPageContent[2]}}</p>
-          </div>
-          <div class="eachHint">
-            <p>{{secondPageContent[3]}}</p>
-            <p>{{secondPageContent[4]}}</p>
-          </div>
-        </div>
-        <div class="topic">
-          {{chapterContent[4]}}
-        </div>
-      </div>
-      <footer>24/7English</footer>
-      <div class="pagebreak"></div>
     </div>
-
-
-
   </div>
 </template>
 
 <script>
 import jsPDF from 'jspdf'
-// import html2canvas from "html2canvas"
+import html2canvas from "html2canvas"
 
 export default {
   data() {
@@ -99,16 +109,18 @@ export default {
 
   },
   methods: {
-  downloadPDF () {
-  const doc = new jsPDF();
-  doc.html(document.frontHeader, {
-   callback: function (doc) {
-     doc.save();
-   },
-   x: 10,
-   y: 10
-});
- }
+    downloadPDF() {
+      const source = this.$refs.pdf;
+      console.log(source);
+      html2canvas(source).then((capture) => {
+        const imgData = capture.toDataURL("image/png");
+        // ここから追記
+        const doc = new jsPDF();
+        const width = doc.internal.pageSize.width;
+        doc.addImage(imgData, "PNG", 0, 0, width * 0.9, 0);
+        doc.save("sa.pdf");
+      });
+    },
 }
 }
 </script>
@@ -145,5 +157,9 @@ export default {
 
 .pagebreak {
   break-after: page;
+}
+
+.intro {
+  font-size: 1px;
 }
 </style>
